8ba3dc1b16aa5ab21cfc0439da39bb76217949dc
ewhillier <eve@whillier.com>
2017-01-31 18:00:32 -0700

    galleries and project pages

--- php-modules/work-menu.php
+++ php-modules/work-menu.php
@@ -9,7 +9,7 @@
 				<h1>Say Goodbye to the Sticky Past</h1>
 				<h2>Parker's Maple<br>packaging, branding, print</h2>
 				<p>A special release design of US currency for the 20th anniversary of September 11th.</p>
-				<a class="button" href="parkers_maple.php">View</a>
+				<a class="button work-menu-button" href="parkers_maple.php">View</a>
 			</div>
 		</div>
 		
@@ -21,7 +21,7 @@
 				<h1>Portfolio<br>Overhaul</h1>
 				<h2>Catapult<br>web design, front-end dev</h2>
 				<p>A special release design of US currency for the 20th anniversary of September 11th.</p>
-				<a class="button" href="catapult.php">View</a>
+				<a class="button work-menu-button" href="catapult.php">View</a>
 			</div>
 		</div>
 		
@@ -33,7 +33,7 @@
 				<h1>Innovation<br>Space</h1>
 				<h2>Honeywell<br>product dev, print, branding</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="innovation_space.php">View</a>
+				<a class="button work-menu-button" href="innovation_space.php">View</a>
 			</div>
 		</div>
 		
@@ -45,7 +45,7 @@
 				<h1>Chill out with Thermodynamics</h1>
 				<h2>ASU<br>logo, research, ~engineering, illustration, print</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="cluster.php">View</a>
+				<a class="button work-menu-button" href="cluster.php">View</a>
 			</div>
 		</div>
 		
@@ -57,7 +57,7 @@
 				<h1>Nature is Smarter Than You</h1>
 				<h2>ASU<br>exhibit materials, print</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="biomimicry.php">View</a>
+				<a class="button work-menu-button" href="biomimicry.php">View</a>
 			</div>
 		</div>
 		
@@ -69,7 +69,7 @@
 				<h1>Mars Poster Series</h1>
 				<h2>ASU<br>print, typography |&nbsp;Award</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="mars_series.php">View</a>
+				<a class="button work-menu-button" href="mars_series.php">View</a>
 			</div>
 		</div>
 		
@@ -81,7 +81,7 @@
 				<h1>Gnat Warfare</h1>
 				<h2>Gnat Warfare<br>logo</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="gnat_warfare.php">View</a>
+				<a class="button work-menu-button" href="gnat_warfare.php">View</a>
 			</div>
 		</div>
 		
@@ -93,7 +93,7 @@
 				<h1>Website Rennovation</h1>
 				<h2>Industrial Power Solutions<br>branding, web design</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="ips.php">View</a>
+				<a class="button work-menu-button" href="ips.php">View</a>
 			</div>
 		</div>
 		
@@ -105,7 +105,7 @@
 				<h1>The Best this Land has to Offer</h1>
 				<h2>Molson Canadian<br>storefront</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="molson_canadian.php">View</a>
+				<a class="button work-menu-button" href="molson_canadian.php">View</a>
 			</div>
 		</div>
 		
@@ -117,7 +117,7 @@
 				<h1>News by the People, for the People</h1>
 				<h2>ASU Now<br>logo, web design</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="asu_now.php">View</a>
+				<a class="button work-menu-button" href="asu_now.php">View</a>
 			</div>
 		</div>
 		
@@ -129,7 +129,7 @@
 				<h1>It Starts with One</h1>
 				<h2> Martin Luther King Jr. Committee<br>campaign</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="mlk_campaign.php">View</a>
+				<a class="button work-menu-button" href="mlk_campaign.php">View</a>
 			</div>
 		</div>
 		
@@ -141,7 +141,7 @@
 				<h1>Focus Right Marketing</h1>
 				<h2>Focus Right Marketing<br>logo</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="frm.php">View</a>
+				<a class="button work-menu-button" href="frm.php">View</a>
 			</div>
 		</div>
 		
@@ -153,7 +153,7 @@
 				<h1>Environmental Design</h1>
 				<h2>PVHS<br>environmental</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="pvhs.php">View</a>
+				<a class="button work-menu-button" href="pvhs.php">View</a>
 			</div>
 		</div>
 		
@@ -165,7 +165,7 @@
 				<h1>A Commemoration of the Free & the Brave</h1>
 				<h2>ASU<br>print</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="currency.php">View</a>
+				<a class="button work-menu-button" href="currency.php">View</a>
 			</div>
 		</div>
 		
@@ -177,7 +177,7 @@
 				<h1>Your Body, Better.</h1>
 				<h2>Landmark Physical Therapy<br>logo</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="landmark.php">View</a>
+				<a class="button work-menu-button" href="landmark.php">View</a>
 			</div>
 		</div>
 		
@@ -189,7 +189,7 @@
 				<h1>Giving Tuesday</h1>
 				<h2>ASU<br>campaign</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="giving_tuesday.php">View</a>
+				<a class="button work-menu-button" href="giving_tuesday.php">View</a>
 			</div>
 		</div>
 		
@@ -201,7 +201,7 @@
 				<h1>Top Knotch</h1>
 				<h2>Boyscouts of America<br>packaging</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="top_knotch.php">View</a>
+				<a class="button work-menu-button" href="top_knotch.php">View</a>
 			</div>
 		</div>
 		
@@ -213,7 +213,7 @@
 				<h1>Major Sound</h1>
 				<h2>Duo Records<br>branding</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="duo.php">View</a>
+				<a class="button work-menu-button" href="duo.php">View</a>
 			</div>
 		</div>
 		
@@ -225,7 +225,7 @@
 				<h1>Township of LaSalle</h1>
 				<h2>LaSalle<br>web design</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="lasalle.php">View</a>
+				<a class="button work-menu-button" href="lasalle.php">View</a>
 			</div>
 		</div>
 		
@@ -237,7 +237,7 @@
 				<h1>Self Promotion</h1>
 				<h2>ASU<br>woodworking, product dev</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="self_promo.php">View</a>
+				<a class="button work-menu-button" href="self_promo.php">View</a>
 			</div>
 		</div>
 		
@@ -249,7 +249,7 @@
 				<h1>Puslinch</h1>
 				<h2>Puslinch<br>web design</h2>
 				<p>Starter kit illustration design and hand-crafted packaging for Boyscouts.</p>
-				<a class="button" href="puslinch.php">View</a>
+				<a class="button work-menu-button" href="puslinch.php">View</a>
 			</div>
 		</div>
 		
