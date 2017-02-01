a69f1289f2d8897153a2308a0a7bea59bf84dada
ewhillier <eve@whillier.com>
2017-01-31 18:56:09 -0700

    project updates

---
+++ gnat_warfare.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link href="_/css/normalize.css" rel="stylesheet" type="text/css">
+	<link href="_/css/main.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+	<title>Duo Records Branding Design</title>
+</head>
+
+<body>
+	
+<! HEADER >
+	<?php include 'php-modules/header.php';?>
+	
+	
+<! MAIN >	
+	<main class="project-main">
+	
+	<div class="project-hero gnat_warfare-project-hero">
+	</div>
+	
+		<section class="project-meta">
+			<div class="project-inner-wrapper">
+				<h1>A Commemoration of the Free & the Brave</h1>
+				<h2>ASU Now</h2>
+				<h3>Arizona State University’s mission to overhaul their news site brought to the forefront their need to rebrand and employ a solid user experience.</h3>
+				<p class="project-details"><span class="category">Role:</span> Visual Design, UI/UX</p>
+				<p class="project-details"><span class="category">Designed at:</span> Arizona State University</p>
+				<p class="project-details"><span class="category">Work:</span> Logo, Responsive Website</p>
+				<p>The ASU Now logo employs the use of the letter ‘o’ to form quotation marks. It’s a reminder that ASU students have a voice, and that their voice is employed through ASU Now.</p>
+				<p>Just as the logo reminds students that they have contol through the ASU Now voice, the website allows users to take contol of their news experience. While the newest and most popular articles appear at the top of the feed, the filters allow users to view news at their campus, in their location, on a specific date, and by topic. The responsive experience enhances the content without sacrificing any features for smaller sizes, giving the site a seamless transition through all devices.</p>
+			</div>	
+		</section>
+	
+		<section class="project-gallery">
+			<img class="gallery-image" src="_/assets/projects/gnat_warfare/gnat_warfare-gallery-01.jpg">
+		</section>
+	
+	</main>
+	
+	<?php include 'php-modules/work-menu.php';?>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+	<script src="_/js/interactions.js"></script>
+	
+	
+</body>
